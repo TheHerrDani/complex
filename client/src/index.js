f1fbfe4865a8bf2f6d4cb652d8f018515f599c90
DESKTOP-BNLR8FO\TheHerrDani <sipos.herr.dani@gmail.com>
2022-04-01 14:04:22 +0200

    Create production dockerfiles

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.createRoot(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
